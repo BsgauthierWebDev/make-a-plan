974cda4e946a0c0facd0ed85d1dd50ff9b96df5a
Brent Gauthier <brentsgauthier@gmail.com>
2021-04-07 18:33:16 -0400

    Built Login Page

---
+++ src/components/LogIn/LogIn.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import Context from '../../context';
+//import ValidationError from '../ValidationError';
+import AuthApiService from '../../services/auth-api-service';
+import TokenService from '../../services/token-service';
+import './SignIn.css';
+
+class LogIn extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            error: null
+        }
+    }
+
+    static defaultProps = {
+        history: {
+            push: () => {}
+        }
+    }
+
+    handleLoginSuccess = () => {
+        const {history} = this.props
+        history.push('/user')
+        TokenService.hasAuthToken()
+    }
+
+    handleSubmitJwtAuth = e => {
+        e.preventDefault()
+        this.setState({error: null})
+        const {username, password} = e.target
+
+        AuthApiService.postLogin({
+            username: username.value,
+            password: password.value
+        })
+            .then(res => {
+                username.value = ''
+                password.value = ''
+                TokenService.saveAuthToken(res.authToken)
+                this.handleLoginSuccess()
+            })
+            .catch(res => {
+                this.setState({error: res.error})
+            })
+    }
+
+    render() {
+        const {error} = this.state
+        return(
+            <div className = 'LogIn'>
+                <div className = 'LogIn__area'>
+                    <h1>Log In</h1>
+                    <form className = 'LogIn__form' onSubmit = {this.handleSubmitJwtAuth}>
+                        <div role = 'alert'>
+                            {error && <p className = 'red'>{error}</p>}
+                        </div>
+                        <div>
+                            <label htmlFor = 'username'>Username: </label>
+                            <input placeholder = 'username' type = 'text' name = 'username' id = 'username'
+                                autoComplete = 'username'
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor = 'password'>Password: </label>
+                            <input type = 'password' name = 'password' id = 'password'
+                                placeholder = 'password' autoComplete = 'current-password'
+                                required
+                            />
+                        </div>
+                        <button type = 'submit'>Log In</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LogIn;
