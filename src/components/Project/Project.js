ccb98f8ad192a3213583043685000285a3cc83d4
Brent Gauthier <brentsgauthier@gmail.com>
2021-04-13 18:27:24 -0400

    Created individual project component

---
+++ src/components/Project/Project.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {Redirect, Link} from 'react-router-dom';
+import {format} from 'date-fns';
+import ApiContext from '../../ApiContext';
+import config from '../../config';
+
+export default class Project extends React.Component {
+    static defaultProps = {
+        onDeleteProject: () => {},
+        history: {
+            push: () => {}
+        },
+    }
+    static contextType = ApiContext;
+
+    handleClickDelete = (e) => {
+        e.preventDefault()
+        const projectId = this.props.id
+        console.log(projectId)
+
+        fetch(`${config.API_ENDPOINT}/api/projects/${projectId}`, {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json'
+            },
+        })
+            .then(() => {
+                console.log(`Deleting the project.`)
+                this.props.history.push('/projects')
+                this.context.deleteProject(projectId)
+            })
+            .catch(error => {
+                console.error({error})
+            })
+    }
+
+    render() {
+        const {name, id, modified} = this.props
+        if (!this.props.id) {
+            return <Redirect to = '/' />
+        }
+        return (
+            <div className = 'Project'>
+                <h2 className = 'Project__title'>
+                    <Link to = {`/projects/${id}`}>
+                        {name}
+                    </Link>
+                </h2>
+                <button
+                    className = 'Project__delete'
+                    type = 'button'
+                    onClick = {this.handleClickDelete}
+                >
+                    Delete
+                </button>
+                <div className = 'Project__dates'>
+                    <div className = 'Project__dates-modified'>
+                        Modified
+                        {' '}
+                        <span className = 'Date'>
+                            {format(new Date(modified), 'MMM dd yyyy')}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
