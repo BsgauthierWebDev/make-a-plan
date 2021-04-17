c7527b2a88a2537311875c3f997ad39529ab22b1
Brent Gauthier <brentsgauthier@gmail.com>
2021-04-17 15:03:12 -0400

    Edited the store for the demo

---
+++ src/Store.js
@@ -0,0 +1,20 @@
+export default {
+    projects:[
+        {
+            id: 1,
+            name: 'Test Name 1',
+            description: 'Test Description 1',
+            materials: 'Test Materials 1',
+            steps: 'Test Steps 1',
+            user_id: 1
+        },
+        {
+            id: 2,
+            name: 'Test Name 2',
+            description: null,
+            materials: 'Test Materials 2',
+            steps: 'Test Steps 2',
+            user_id: 1
+        }
+    ]
+}
