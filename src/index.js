f4bd7af39620dfee6d704a61c32f160c1ff86f14
Brent Gauthier <brentsgauthier@gmail.com>
2021-05-16 16:20:44 -0400

    Importing icons from Fort Awesome

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faPlus, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
 import {BrowserRouter} from 'react-router-dom';
 import App from './components/App/App';
 import './index.css';
 import {ProjectListProvider} from './context';
+
+library.add(faPlus, faTrashAlt)
 
 ReactDOM.render(
     <BrowserRouter>
