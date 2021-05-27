45f877ebe9e829cbd1f5c45ea014c98d3a7c3520
Brent Gauthier <brentsgauthier@gmail.com>
2021-05-27 15:16:26 -0400

    Changed API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://pure-castle-46870.herokuapp.com/api',
+    API_ENDPOINT: 'https://infinite-savannah-38674.herokuapp.com/api',
     API_KEY: process.env.API_KEY,
     TOKEN_KEY: 'make-a-plan-auth-token',
     NODE_ENV: process.env.NODE_ENV || 'production'
