09f0c867f77d845655a0ddfedbc1e803d454b7e9
Brent Gauthier <brentsgauthier@gmail.com>
2021-05-27 12:38:20 -0400

    Changed API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://nameless-coast-57604.herokuapp.com/api',
+    API_ENDPOINT: 'https://pure-castle-46870.herokuapp.com/api',
     API_KEY: process.env.API_KEY,
     TOKEN_KEY: 'make-a-plan-auth-token',
     NODE_ENV: process.env.NODE_ENV || 'production'
